f617cd00ae089ca49691b3a94a62bc020845dc20
kazuma-fujita-genova <kazuma.fujita@genova.co.jp>
2018-12-21 12:10:00 +0900

    Initial Commit

---
+++ SearchKeywordViewPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SearchKeywordViewPractice
+//
+//  Created by 藤田和磨 on 2018/12/21.
+//  Copyright © 2018 藤田和磨. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
